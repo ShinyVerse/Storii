8b9dd1bd0d465fde62d31c0e39cab9a2a3c31931
Laura Jackson <playeronereadystart@gmail.com>
2020-05-26 18:14:15 +0100

    begin form tests

--- src/components/Form/Form.test.js
+++ src/components/Form/Form.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import { Form } from "./Form"
 
@@ -7,15 +7,59 @@ describe('Form', () => {
   const onSubmitSpy = jest.fn()
   const defaultProps = {
     handleSubmit: onSubmitSpy,
-    initState: {}
+    initState: {
+      testInput: ""
+    }
   }
-  it.only('renders form container to page', () => {
-    const wrapper = shallow(<Form {...defaultProps}>{() => {}}</Form>);
-    const formContainer = wrapper.find('form');
+  it('renders form container to page', () => {
+    const wrapper = shallow(
+      <Form {...defaultProps}>
+        {() => {}}
+      </Form>);
+    const formElement = wrapper.find('form');
 
-    expect(formContainer.length).toBe(1)
+    expect(formElement.length).toBe(1)
   });
-  
+
+  it('can render children', () => {
+    const wrapper = shallow(
+      <Form {...defaultProps}>
+        {() => {
+          return <div data-test="test">hello</div>
+        }}
+      </Form>);
+
+    const divElement = wrapper.find('[data-test="test"]');
+
+    expect(divElement.length).toBe(1);
+    expect(divElement.text()).toBe("hello");
+  });
+
+  it('can trigger onChange event ', () => {
+
+    const wrapper = shallow(
+      <Form {...defaultProps}>
+        {({state, onChange}) => {
+          return <input 
+          data-test="input"
+          name="testInput"
+          value={state.div || "hello"}
+          onChange={onChange}
+         />
+        }}
+      </Form>);
+
+    let inputElement = wrapper.find('[data-test="input"]');
+    
+    inputElement.simulate('change', {
+      target: { value: 'byebye' }
+    })
+    wrapper.update();
+
+    inputElement = wrapper.find('[data-test="input"]');
+
+    expect(inputElement.text()).toBe("hello");
+  });
 });
 
 
