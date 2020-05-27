486c72edca47090e06b986d75f689a51b92e7983
Nimzyow <n_soufiani@hotmail.com>
2020-05-27 16:41:52 +0100

    Refactored Form.test and tests passed on checkbox test!

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,52 +1,55 @@
-import React, {useState} from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 export const Form = (props) => {
-  
-  const [state, setState] = useState(props.initState)
+  const [state, setState] = useState(props.initState);
 
   const onChange = (e) => {
     setState({
       ...state,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
-  }
+  };
 
   const onCheckboxChange = (e) => {
-      const {name, checked} = e.target;
-      const arrayName = e.target.attributes.belongsto.value;
-      const array = state[arrayName];
+    const { name, checked } = e.target;
+    const arrayName = e.target.attributes.belongsto.value;
+    const array = state[arrayName];
 
-      if (checked) {
-        array.push(name)
+    if (checked) {
+      array.push(name);
 
-        onChange({target: {name: arrayName, value: array}});
-        return;
-      }
-      array = array.filter(item => item !== name);
-     
-      onChange({target: {name: arrayName, value: array}});
+      onChange({ target: { name: arrayName, value: array } });
       return;
-  }
-  
-  const onSubmit = (e) => {
-    e.preventDefault()
-    props.handleSubmit(state)
-  }
+    }
+    array = array.filter((item) => item !== name);
+
+    onChange({ target: { name: arrayName, value: array } });
+    return;
+  };
+
+  const onSubmit = () => {
+    props.handleSubmit(state);
+  };
 
   return (
-    <form onSubmit={onSubmit}>
-     {props.children({
-       state,
-       onChange,
-       onCheckboxChange
-     })}
-     <input type="submit" value="SUBMIT ME"/>
+    <form>
+      {props.children({
+        state,
+        onChange,
+        onCheckboxChange,
+      })}
+      <input
+        onClick={onSubmit}
+        data-test="submit"
+        type="submit"
+        value="SUBMIT ME"
+      />
     </form>
-  )
-}
+  );
+};
 
 Form.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
   initState: PropTypes.object.isRequired,
-}
+};
--- src/components/Form/Form.test.js
+++ src/components/Form/Form.test.js
@@ -1,38 +1,34 @@
-import React from 'react';
-import { shallow, mount } from 'enzyme';
+import React from "react";
+import { shallow, mount } from "enzyme";
 
-import { Form } from "./Form"
+import { Form } from "./Form";
 
-describe('Form', () => {
-  const onSubmitSpy = jest.fn()
-  let defaultProps; 
+describe("Form", () => {
+  const onSubmitSpy = jest.fn();
+  let defaultProps;
 
   beforeEach(() => {
+    jest.clearAllMocks();
     defaultProps = {
       handleSubmit: onSubmitSpy,
-      initState: {
-        
-      }
-    }
-  })
-  it('renders form container to page', () => {
+      initState: {},
+    };
+  });
+  it("renders form container to page", () => {
+    const wrapper = shallow(<Form {...defaultProps}>{() => {}}</Form>);
+    const formElement = wrapper.find("form");
 
-    const wrapper = shallow(
-      <Form {...defaultProps}>
-        {() => {}}
-      </Form>);
-    const formElement = wrapper.find('form');
-
-    expect(formElement.length).toBe(1)
+    expect(formElement.length).toBe(1);
   });
 
-  it('can render children', () => {
+  it("can render children", () => {
     const wrapper = shallow(
       <Form {...defaultProps}>
         {() => {
-          return <div data-test="test">hello</div>
+          return <div data-test="test">hello</div>;
         }}
-      </Form>);
+      </Form>
+    );
 
     const divElement = wrapper.find('[data-test="test"]');
 
@@ -40,81 +36,114 @@ describe('Form', () => {
     expect(divElement.text()).toBe("hello");
   });
 
-  it('can trigger onChange event on input', () => {
+  it("can trigger onChange event on input", () => {
     defaultProps.initState.testInput = "";
 
     const wrapper = shallow(
       <Form {...defaultProps}>
-        {({state, onChange}) => {
-          return (<input 
-          data-test="testInput"
-          name="testInput"
-          value={state.testInput || "hello"}
-          onChange={onChange}
-         />)
+        {({ state, onChange }) => {
+          return (
+            <input
+              data-test="testInput"
+              name="testInput"
+              value={state.testInput || "hello"}
+              onChange={onChange}
+            />
+          );
         }}
-      </Form>);
-      
-    let inputElement = wrapper.find('[data-test="testInput"]')
-      
-    inputElement.simulate('change', 
-      { target: { name: 'testInput', value: 'CHANGED' }}
-    )
+      </Form>
+    );
 
-    inputElement = wrapper.find('[data-test="testInput"]')
+    let inputElement = wrapper.find('[data-test="testInput"]');
 
-    expect(inputElement.prop('value')).toBe('CHANGED');
+    inputElement.simulate("change", {
+      target: { name: "testInput", value: "CHANGED" },
+    });
+
+    inputElement = wrapper.find('[data-test="testInput"]');
+
+    expect(inputElement.prop("value")).toBe("CHANGED");
   });
 
-  it('can trigger onChange event with a dropdown', () => {
+  it("can trigger onChange event with a dropdown", () => {
     defaultProps.initState.selection = "one";
 
-    const selection = ['one', 'two', 'three'];
+    const selection = ["one", "two", "three"];
     const wrapper = shallow(
       <Form {...defaultProps}>
-        {({state, onChange}) => {
-
+        {({ state, onChange }) => {
           return (
-            <select 
-            value={state.selection} 
-            data-test="select"
-            name="selection"
-            onChange={onChange}>
-            {selection.map(option => <option key={option} value={option}>{option}</option>)}
-          </select>
-          )
+            <select
+              value={state.selection}
+              data-test="select"
+              name="selection"
+              onChange={onChange}
+            >
+              {selection.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          );
         }}
-      </Form>);
-      
+      </Form>
+    );
+
     let selectElement = wrapper.find('[data-test="select"]');
 
-    selectElement.simulate('change', 
-      { target: { name: 'selection', value: selection[1] }}
-    )
+    selectElement.simulate("change", {
+      target: { name: "selection", value: selection[1] },
+    });
 
-    selectElement = wrapper.find('[data-test="select"]')
+    selectElement = wrapper.find('[data-test="select"]');
 
-    expect(selectElement.prop('value')).toBe('two');
+    expect(selectElement.prop("value")).toBe("two");
   });
 
-  it('can trigger onChange event with checkboxes', () => {
-     
-  });
+  it("can trigger onCheckBoxChange event with checkboxes", () => {});
 
-  it('returns current state on submitting', () => {
-     
+  it("returns current state on submitting", () => {
+    defaultProps.initState.testInput = "";
+
+    const wrapper = shallow(
+      <Form {...defaultProps}>
+        {({ state, onChange }) => {
+          return (
+            <input
+              data-test="testInput"
+              name="testInput"
+              value={state.testInput || "hello"}
+              onChange={onChange}
+            />
+          );
+        }}
+      </Form>
+    );
+
+    const inputElement = wrapper.find('[data-test="testInput"]');
+
+    inputElement.simulate("change", {
+      target: { name: "testInput", value: "CHANGED" },
+    });
+
+    const submitElement = wrapper.find('[data-test="submit"]');
+
+    submitElement.simulate("click");
+
+    expect(onSubmitSpy).toHaveBeenCalledTimes(1);
+    expect(onSubmitSpy).toHaveBeenCalledWith({ testInput: "CHANGED" });
   });
 });
 
-
 // Login/Regsiter form
-  // penname
-  // email
-  // password
-  // password2
+// penname
+// email
+// password
+// password2
 
 // new Storii form
-  // title
-  // description
-  // mainGenre (DROPDOWN)
-  // tags (CHECKBOX)
+// title
+// description
+// mainGenre (DROPDOWN)
+// tags (CHECKBOX)
