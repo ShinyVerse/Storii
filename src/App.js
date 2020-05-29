79c8a4a89d5b4d6bd0607d95fdf0f029fbae6a45
Laura Jackson <playeronereadystart@gmail.com>
2020-05-29 11:50:49 +0100

    start landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import StoryContainer from './components/StoryPage/StoryContainer';
-import EntryForm from './components/StoryPage/EntryForm';
+import React, { useEffect, useState } from "react";
+import StoryContainer from "./components/StoryPage/StoryContainer";
+import EntryForm from "./components/StoryPage/EntryForm";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './App.scss'
+import "./App.scss";
 
-import {LandingPage} from "./Pages/LandingPage/LandingPage";
+import { LandingPage } from "./Pages/LandingPage/LandingPage";
 
 const socket = new WebSocket("wss://javascript.info/article/websocket/chat/ws");
 
 export default function App() {
-  console.log('HI! I AM APP')
-  const [story, updateStory] = useState([])
-  
-  useEffect(() => {    console.log('HI!')
+  const [story, updateStory] = useState([]);
+
+  useEffect(() => {
+    console.log("HI!");
     console.log(story);
-  }, [story]); 
+  }, [story]);
 
   socket.onmessage = (event) => {
     updateStory([...story, event.data]);
   };
-
 
   // socket.onopen = () => {
   //   console.log("[open] Connection established");
@@ -32,35 +31,35 @@ export default function App() {
     //listen for any incoming entries
     //if new entry by other writer then show on page.
     socket.send(entry);
-  }
+  };
   //My name is Nima
 
+  // socket.onclose = function(event) {
+  //   if (event.wasClean) {
+  //     alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+  //   } else {
+  //     // e.g. server process killed or network down
+  //     // event.code is usually 1006 in this case
+  //     alert('[close] Connection died');
+  //   }
+  // };
 
+  socket.onerror = function (error) {
+    alert(`[error] ${error.message}`);
+  };
 
-// socket.onclose = function(event) {
-//   if (event.wasClean) {
-//     alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-//   } else {
-//     // e.g. server process killed or network down
-//     // event.code is usually 1006 in this case
-//     alert('[close] Connection died');
-//   }
-// };
-
-socket.onerror = function(error) {
-  alert(`[error] ${error.message}`);
-};
-
-{/* <h1 style={{color: "black"}}>Hello world!</h1>
+  {
+    /* <h1 style={{color: "black"}}>Hello world!</h1>
       <StoryContainer story={story}/>
-      <EntryForm onSubmit={(entry) => {addEntryToStory(entry)} }/> */}
+      <EntryForm onSubmit={(entry) => {addEntryToStory(entry)} }/> */
+  }
   return (
-    <div id='app'>
+    <div id="app">
       <Router>
         <Switch>
           <Route exact path="/" component={LandingPage} />
         </Switch>
       </Router>
     </div>
-  )
+  );
 }
--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,99 +1,153 @@
-import React from 'react'
-import { HeaderWithText } from "../../components/HeaderWithText/HeaderWithText"
-import { Image } from "../../components/Image/Image"
-import { Form } from "../../components/Form/Form"
+import React, { useState } from "react";
+import { HeaderWithText } from "../../components/HeaderWithText/HeaderWithText";
+import { Image } from "../../components/Image/Image";
+import { Form } from "../../components/Form/Form";
 
-import img from '../../assets/img/iconLong.png';
+import img from "../../assets/img/iconLong.png";
 
 export const LandingPage = () => {
-  {/* <Image 
-        src={img}
-        type='long'
-        alt='storii join instructions'
-      /> */}
+  const [formType, setFormType] = useState("register");
 
-    const handleSubmit = (state) => {
-      console.log("EXAMPLE OF BIG BOY", state);
+  const checkFormIsPopulated = (state) => {
+    for (let [key, value] of Object.entries(state)) {
+      if (value === "") {
+        return false;
+      }
     }
+    return true;
+  };
 
-   
+  const checkPassword = ({ password, password2 }) => {
+    if (password === password2) {
+      return true;
+    }
+    return false;
+  };
+
+  const handleSubmit = (state) => {
+    if (!checkFormIsPopulated(state)) {
+      alert("you need to fill in the form");
+      return;
+    }
+    if (formType === "register") {
+      if (!checkPassword(state)) {
+        alert("PASSWORDS NOT THE SAME");
+        return;
+      }
+    }
+    console.log("EXAMPLE OF BIG BOY", state);
+  };
 
   return (
     <div>
-      <HeaderWithText header='Storii'/>
-      <Form
-        initState = {{
-          // penName: "",
-          // email:"",
-          // password: "",
-          // password2: "",
-          // selection: "one",
-          tags: []
-        }}
-        handleSubmit={handleSubmit}
-      >
-        {
-          ({state, onCheckboxChange}) => { 
-            const {penName, email, password, password2} = state;
+      <HeaderWithText header="Storii" />
+      <Image src={img} type="long" alt="storii join instructions" />
 
-            return(
-              <div>
+      {formType === "register" && (
+        <div>
+          <Form
+            initState={{
+              penName: "",
+              email: "",
+              password: "",
+              password2: "",
+            }}
+            btnName="create"
+            handleSubmit={handleSubmit}
+          >
+            {({ state, onChange }) => {
+              const { penName, email, password, password2 } = state;
 
-             <label>
-                fantasy:
-                <input
-                  name="fantasy"
-                  type="checkbox"
-                  belongsto="tags"
-                  onChange={onCheckboxChange} />
-              </label>
-              <label>
-                sci-fi:
-                <input
-                  name="sci-fi"
-                  type="checkbox"
-                  belongsto="tags"
-                  onChange={onCheckboxChange} />
-              </label>
-     
-                  
-            {/* <select 
-              value={state.selection} 
-              data-test="select"
-              name="selection"
-              onChange={onChange}>
-              {selection.map(option => <option key={option} value={option}>{option}</option>)}
-            </select> */}
-                {/* <input 
-                name="penName" 
-                onChange={onChange} 
-                value={penName || ""}
-                type="text"
-                />
-                <input 
-                name="email" 
-                onChange={onChange} 
-                value={email || ""}
-                type="email"
-                />
-                <input 
-                name="password" 
-                onChange={onChange} 
-                value={password || ""}
-                type="text"
-                />
-                <input 
-                name="password2" 
-                onChange={onChange} 
-                value={password2 || ""}
-                type="text"
-                /> */}
-            </div>
-            )
-           }  
-        }
-      </Form>
+              return (
+                <div>
+                  <label htmlFor="penName">Pen Name:</label>
+                  <input
+                    name="penName"
+                    onChange={onChange}
+                    value={penName || ""}
+                    type="text"
+                    required
+                  />
+                  <label htmlFor="email">Email:</label>
+                  <input
+                    name="email"
+                    onChange={onChange}
+                    value={email || ""}
+                    type="email"
+                    required
+                  />
+                  <label htmlFor="">Password:</label>
+                  <input
+                    name="password"
+                    onChange={onChange}
+                    value={password || ""}
+                    type="password"
+                    required
+                    minLength="6"
+                  />
+                  <label htmlFor="password2">Confirm password:</label>
+                  <input
+                    name="password2"
+                    onChange={onChange}
+                    value={password2 || ""}
+                    type="password"
+                    required
+                    minLength="6"
+                  />
+                  <p>
+                    Already a writer?
+                    <a onClick={() => setFormType("login")}>Click here</a>
+                  </p>
+                </div>
+              );
+            }}
+          </Form>
+        </div>
+      )}
+
+      {formType === "login" && (
+        <div>
+          <Form
+            initState={{
+              email: "",
+              password: "",
+            }}
+            btnName="login"
+            handleSubmit={handleSubmit}
+          >
+            {({ state, onChange }) => {
+              const { email, password } = state;
+
+              return (
+                <div>
+                  <label htmlFor="email">Email:</label>
+                  <input
+                    name="email"
+                    onChange={onChange}
+                    value={email || ""}
+                    type="email"
+                    required
+                  />
+                  <label htmlFor="">Password:</label>
+                  <input
+                    name="password"
+                    onChange={onChange}
+                    value={password || ""}
+                    type="password"
+                    required
+                    minLength="6"
+                  />
+
+                  <p>
+                    Not yet a writer?
+                    <a onClick={() => setFormType("register")}>Click here</a>
+                  </p>
+                </div>
+              );
+            }}
+          </Form>
+        </div>
+      )}
     </div>
-  )
-}
-
+  );
+};
