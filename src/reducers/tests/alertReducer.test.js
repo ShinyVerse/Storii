7a494d41e2354f803a58564554eb3eb9d369c948
Laura Jackson <playeronereadystart@gmail.com>
2020-06-03 16:21:17 +0100

    add alertReducer

--- src/reducers/tests/alertReducer.test.js
+++ src/reducers/tests/alertReducer.test.js
@@ -11,10 +11,11 @@ describe("alertReducer", () => {
       type: "unrelated action",
     };
 
-    expect(authReducer(dummyCurrentState, fakeAction)).toEqual(
-      dummyCurrentState
+    expect(alertReducer(dummyCurrentState, fakeAction)).toEqual(
+      dummyCurrentState,
     );
   });
+
   it("update state when setAlert is called", () => {
     const action = {
       type: types.SET_ALERT,
@@ -22,23 +23,28 @@ describe("alertReducer", () => {
     };
 
     const expectedState = {
-      alert: action.payload,
+      message: action.payload,
     };
 
     const state = alertReducer(undefined, action);
 
     expect(state).toEqual(expectedState);
   });
+
   it("update state when clearAlert is called", () => {
+    const initialState = {
+      message: "ALERT",
+    };
+
     const action = {
       type: types.CLEAR_ALERT,
     };
 
     const expectedState = {
-      alert: null,
+      message: null,
     };
 
-    const state = alertReducer(undefined, action);
+    const state = alertReducer(initialState, action);
 
     expect(state).toEqual(expectedState);
   });
