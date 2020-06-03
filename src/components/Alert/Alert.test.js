40c9de923d804049fb42011072ed66c6a3fd9077
Nimzyow <n_soufiani@hotmail.com>
2020-06-03 17:07:13 +0100

    alert component created

--- src/components/Alert/Alert.test.js
+++ src/components/Alert/Alert.test.js
@@ -5,14 +5,15 @@ import { Alert } from "./Alert";
 
 describe("Alert", () => {
   it("displays when alert has a message", () => {
-    const wrapper = shallow(<Alert props={{ alert: { message: "Alert!" } }} />);
+    const wrapper = shallow(<Alert alert={{ message: "Alert!" }} />);
     const alertElement = wrapper.find('[data-test="alert"]');
 
     expect(alertElement.length).toBe(1);
+    expect(alertElement.text()).toBe("Alert!");
   });
 
   it("doesn't display when alert.message is null", () => {
-    const wrapper = shallow(<Alert props={{ alert: { message: null } }} />);
+    const wrapper = shallow(<Alert alert={{ message: null }} />);
     const alertElement = wrapper.find('[data-test="alert"]');
 
     expect(alertElement.length).toBe(0);
