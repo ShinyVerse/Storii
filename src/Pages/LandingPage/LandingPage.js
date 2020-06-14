288391d9e01e58c8a48e776b1012052131e67fad
Nimzyow <n_soufiani@hotmail.com>
2020-06-14 19:53:45 +0100

    form farm created and refactoring

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -7,6 +7,7 @@ import img from "../../assets/img/iconLong.png";
 
 import { connect } from "react-redux";
 
+import { registerForm, loginForm } from "../../formFarm/FormFarm";
 import { registerUser, loginUser } from "../../actions/auth";
 import { setAlert } from "../../actions/alert";
 
@@ -54,109 +55,11 @@ const LandingPage = ({ registerUser, setAlert, loginUser, history, token }) => {
       <Image src={img} type="long" alt="storii join instructions" />
 
       {formType === "register" && (
-        <div>
-          <Form
-            initState={{
-              penName: "laursy",
-              email: "laursy@laura.com",
-              password: "123456",
-              password2: "123456",
-            }}
-            btnName="create"
-            handleSubmit={handleSubmit}
-          >
-            {({ state, onChange }) => {
-              const { penName, email, password, password2 } = state;
-
-              return (
-                <div>
-                  <label htmlFor="penName">Pen Name:</label>
-                  <input
-                    name="penName"
-                    onChange={onChange}
-                    value={penName}
-                    type="text"
-                    required
-                  />
-                  <label htmlFor="email">Email:</label>
-                  <input
-                    name="email"
-                    onChange={onChange}
-                    value={email}
-                    type="email"
-                    required
-                  />
-                  <label htmlFor="">Password:</label>
-                  <input
-                    name="password"
-                    onChange={onChange}
-                    value={password}
-                    type="password"
-                    required
-                    minLength="6"
-                  />
-                  <label htmlFor="password2">Confirm password:</label>
-                  <input
-                    name="password2"
-                    onChange={onChange}
-                    value={password2}
-                    type="password"
-                    required
-                    minLength="6"
-                  />
-                  <p>
-                    Already a writer?
-                    <a onClick={() => setFormType("login")}>Click here</a>
-                  </p>
-                </div>
-              );
-            }}
-          </Form>
-        </div>
+        <div>{registerForm(handleSubmit, setFormType)}</div>
       )}
 
       {formType === "login" && (
-        <div>
-          <Form
-            initState={{
-              email: "laursy@laura.com",
-              password: "123456",
-            }}
-            btnName="login"
-            handleSubmit={handleSubmit}
-          >
-            {({ state, onChange }) => {
-              const { email, password } = state;
-
-              return (
-                <div>
-                  <label htmlFor="email">Email:</label>
-                  <input
-                    name="email"
-                    onChange={onChange}
-                    value={email}
-                    type="email"
-                    required
-                  />
-                  <label htmlFor="">Password:</label>
-                  <input
-                    name="password"
-                    onChange={onChange}
-                    value={password}
-                    type="password"
-                    required
-                    minLength="6"
-                  />
-
-                  <p>
-                    Not yet a writer?
-                    <a onClick={() => setFormType("register")}>Click here</a>
-                  </p>
-                </div>
-              );
-            }}
-          </Form>
-        </div>
+        <div>{loginForm(handleSubmit, setFormType)}</div>
       )}
     </div>
   );
