7cba8bf74494b1accaa13292af64de097f936f34
Laura Jackson <playeronereadystart@gmail.com>
2020-06-19 13:56:00 +0100

    change

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -23,7 +23,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       "process.env": {
-        YOUR_VARIABLE: JSON.stringify(process.env.YOUR_VARIABLE),
+        STORII_SERVER_URL: JSON.stringify(process.env.STORII_SERVER_URL),
       },
     }),
   ],
