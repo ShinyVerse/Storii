8edd106062c7f11fb7e14b27c41b019dec96caed
Laura Jackson <playeronereadystart@gmail.com>
2020-06-19 14:35:24 +0100

    change

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
+    publicPath: __dirname + "/",
   },
   devServer: {
     hot: true,
