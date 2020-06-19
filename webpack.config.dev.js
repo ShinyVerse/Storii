279f323f0b6ef58a156baafbdcd16bb1753547b7
Laura Jackson <playeronereadystart@gmail.com>
2020-06-19 13:04:50 +0100

    heroku test

---
+++ webpack.config.dev.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const webpack = require("webpack");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  devServer: {
+    hot: true,
+    contentBase: path.join(__dirname, "dist"),
+    historyApiFallback: true,
+  },
+  plugins: [new Dotenv()],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: "url-loader?name=dist/images/[name].[ext]",
+      },
+    ],
+  },
+};
---
+++ webpack.config.prod.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const webpack = require("webpack");
+// const Dotenv = require("dotenv-webpack");
+require("dotenv").config();
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  devServer: {
+    hot: true,
+    contentBase: path.join(__dirname, "dist"),
+    historyApiFallback: true,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        YOUR_VARIABLE: JSON.stringify(process.env.YOUR_VARIABLE),
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: "url-loader?name=dist/images/[name].[ext]",
+      },
+    ],
+  },
+};
