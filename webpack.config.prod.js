d697f684ae7b255294122a13c22a5a19dfb1e0ad
Laura Jackson <playeronereadystart@gmail.com>
2020-06-20 15:22:20 +0100

    add env var

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -22,6 +22,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       "process.env": {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
         STORII_SERVER_URL: JSON.stringify(process.env.STORII_SERVER_URL),
       },
     }),
