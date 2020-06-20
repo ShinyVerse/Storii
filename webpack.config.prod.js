160124407106392d93bfe6c2bc355d058aaa7eb4
Laura Jackson <playeronereadystart@gmail.com>
2020-06-20 14:24:40 +0100

    check package.json change

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: __dirname + "/",
+    // publicPath: __dirname + "/",
   },
   devServer: {
     hot: true,
